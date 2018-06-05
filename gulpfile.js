13b92f9ab583844fd727a42b6836712613467bef
DESKTOP-BE6T737\xiawa <xiawang1024@icloud.com>
2018-06-05 17:46:00 +0800

    feat:录音上限自动停止回调

--- gulpfile.js
+++ gulpfile.js
@@ -98,9 +98,9 @@ gulp.task('wx-js', () => {
 			.src([
 				`${baseDir}/js/zepto.js`,
 				`${baseDir}/js/fastclick.js`,
+				`${baseDir}/js/weui.js`,
 				`${baseDir}/js/jweixin.js`,
 				`${baseDir}/js/wxshare.js`,
-				`${baseDir}/js/weui.js`,
 				`${baseDir}/js/index.js`
 			])
 			.pipe(sourceMaps.init())
