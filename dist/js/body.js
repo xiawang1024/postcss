3e0dd78553ae53dc9f49f934df9cb1f983d69648
DESKTOP-BE6T737\xiawa <xiawang1024@icloud.com>
2018-06-06 15:21:36 +0800

    feat:改版

--- dist/js/body.js
+++ dist/js/body.js
@@ -1,3 +1,13 @@
+if ('addEventListener' in document) {
+	document.addEventListener(
+		'DOMContentLoaded',
+		function() {
+			FastClick.attach(document.body);
+		},
+		false
+	);
+}
+
 (function() {
 	//播放
 	var audio = document.getElementById('audio');
@@ -23,41 +33,143 @@
 	$('#sendMsg').click(function() {
 		var comment = $('#comment').val();
 		if (!comment) {
-			toast('内容不能为空');
+			weui.alert('内容不能为空');
 		} else {
-			toast(comment);
+			var loading = weui.loading('提交中...');
+			getUser(function(userInfo) {
+				postMsg(userInfo, function() {
+					loading.hide();
+					weui.toast('评论成功，等待审核！');
+					$('#comment').val('');
+				});
+			});
 		}
 	});
+	var CID = '1001';
+	function getUser(cb) {
+		var userInfo = {};
+		var openId = getQueryString('openId');
 
+		$.ajax({
+			type: 'get',
+			url: 'http://a.weixin.hndt.com/user/find/openid?openid=' + openId,
+			dataType: 'json',
+			success: function(data) {
+				if (data.status == 1) {
+					userInfo.creater = data.data.name;
+					userInfo.fromUid = data.data.id;
+					cb && cb(userInfo);
+				} else {
+					console.log('error');
+				}
+			}
+		});
+	}
+	function postMsg(userInfo, cb) {
+		var content = $('#comment').val();
+		var postData = {
+			page: '0',
+			cid: CID,
+			creater: userInfo.creater || '',
+			fromUid: userInfo.fromUid || '',
+			content: content
+		};
+		$.ajax({
+			type: 'post',
+			url: 'http://talk.hndt.com/test/upRadio.do',
+			data: postData,
+			success: function(data) {
+				cb && cb();
+			}
+		});
+	}
+	var page = 1;
+	getComment(function(data) {
+		var list = data.list;
+		result = toHtml(list);
+		$('.comment-list .list-wrap').append(result);
+	});
+	function getComment(cb) {
+		var loading0 = weui.loading('努力加载中...');
+		$.ajax({
+			type: 'post',
+			url: 'http://talk.hndt.com/test/upRadio.do',
+			data: { page: page, cid: CID },
+			success: function(data) {
+				loading0.hide();
+				if (data.result) {
+					cb && cb(data.result);
+				} else {
+					weui.alert('没有更多数据！');
+				}
+			}
+		});
+	}
+	function getQueryString(query) {
+		var qs = location.search.substr(1), // 获取url中"?"符后的字串
+			args = {}, // 保存参数数据的对象
+			items = qs.length ? qs.split('&') : [], // 取得每一个参数项,
+			item = null,
+			len = items.length;
+
+		for (var i = 0; i < len; i++) {
+			item = items[i].split('=');
+			var name = decodeURIComponent(item[0]),
+				value = decodeURIComponent(item[1]);
+			if (name) {
+				args[name] = value;
+			}
+		}
+		return args[query];
+	}
 	//点赞
 	$('.g-bd .m-comment .comment-list .list .zan').click(function() {
 		toast('点赞成功！');
 	});
+
 	//查看更多
 	$('.g-bd .m-comment .comment-list .u-see-more').click(function() {
-		toast('查看更多');
+		page++;
+		var result = '';
+		getComment(function(data) {
+			var list = data.list;
+			result = toHtml(list);
+			$('.comment-list .list-wrap').append(result);
+		});
 	});
 
-	var getSingle = function(fn) {
-		var result;
-		return function() {
-			result || (result = fn.apply(this, arguments));
-		};
-	};
-	var createMsg = function() {
-		var node = $('<div id="toast"></div>');
-		node.css('display', 'none');
-		$('body').append(node);
-		return node;
-	};
-	var createSingleMsg = getSingle(createMsg);
-	function toast(msg) {
-		var node = createSingleMsg(msg);
-		$('#toast').html(msg);
-		$('#toast').css('display', 'block');
-		clearTimeout(timeId);
-		var timeId = setTimeout(function() {
-			$('#toast').css('display', 'none');
-		}, 2000);
+	function toHtml(list) {
+		var html = '';
+		for (var i = 0; i < list.length; i++) {
+			var item = list[i].comment;
+			html +=
+				'<li class="list">' +
+				'   <div class="avatar-wrap">' +
+				'       <img src="' +
+				item.icon +
+				'" alt="" class="img">' +
+				'   </div>' +
+				'   <div class="text-wrap">' +
+				'       <div class="cont-wrap">' +
+				'           <span class="name">' +
+				item.creater +
+				'：</span>' +
+				'           <p class="cont">' +
+				item.content +
+				'</p>' +
+				'       </div>' +
+				'       <div class="other-wrap clearfix">' +
+				'           <span class="time">' +
+				parseDate(item.create_time) +
+				'</span>' +
+				'       </div>' +
+				'   </div>' +
+				' </li>';
+		}
+
+		return html;
+	}
+	function parseDate(nS) {
+		return new Date(parseInt(nS)).toLocaleString().replace(/:\d{1,2}$/, ' ');
 	}
 })();
--- dist/js/index.js
+++ dist/js/index.js
@@ -5,8 +5,53 @@ $(function() {
 		var index = $(this).index();
 		$('.g-bd .m-tab-wrap').eq(index).css('display', 'block').siblings('.m-tab-wrap').css('display', 'none');
 	});
-});
 
-$('.g-bd .products-list .u-see-more').click(function() {
-	alert('查看更多');
+	var page = 2;
+
+	$('.g-bd .products-list .u-see-more').click(function() {
+		var loading = weui.loading('加载中...');
+		$.ajax({
+			url: 'http://api.hndt.com/api/page?template_id=346&page=' + page,
+			dataType: 'json',
+			success: function(data) {
+				if (data.list) {
+					var result = toHtml(data.list);
+					$('.products-list .list-wrap').append(result);
+					page++;
+					loading.hide();
+				} else {
+					loading.hide();
+					weui.alert('没有更多数据！');
+				}
+			}
+		});
+	});
+
+	function toHtml(list) {
+		var html = '';
+		for (var i = 0; i < list.length; i++) {
+			var item = list[i];
+			html +=
+				'<li class="list">' +
+				'    <a href="' +
+				item.link +
+				'">' +
+				'        <div class="img-wrap">' +
+				'            <img src="' +
+				item.src +
+				'" alt="" class="img">' +
+				'        </div>' +
+				'        <div class="text-wrap">' +
+				'            <h3 class="title">' +
+				item.title +
+				'</h3>' +
+				'            <p class="name">' +
+				item.author +
+				'</p>' +
+				'        </div>' +
+				'    </a>' +
+				'</li>';
+		}
+		return html;
+	}
 });
