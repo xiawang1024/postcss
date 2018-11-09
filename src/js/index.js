b6ca5a35d407ee89d7045e0d8e4371d651041d28
wangxia/work <xiawang1024@icloud.com>
2018-11-09 17:38:56 +0800

    页面架构

--- src/js/index.js
+++ src/js/index.js
@@ -1,145 +1,170 @@
 !(function() {
-	var loading = weui.loading('加载中...');
-	if (weChat.isPhone()) {
-		// fastClick 消除click 300ms延迟
-		if ('addEventListener' in document) {
-			document.addEventListener(
-				'DOMContentLoaded',
-				function() {
-					FastClick.attach(document.body);
-				},
-				false
-			);
-		}
-	}
+  var loading = weui.loading('加载中...')
+  if (weChat.isPhone()) {
+    // fastClick 消除click 300ms延迟
+    if ('addEventListener' in document) {
+      document.addEventListener(
+        'DOMContentLoaded',
+        function() {
+          FastClick.attach(document.body)
+        },
+        false
+      )
+    }
+  }
 
-	//判断是否是微信客户端打开
-	if (weChat.isWeiXin()) {
-		var weChatCode = weChat.getQueryString('code');
-		if (weChatCode) {
-			getOpenId(weChatCode, function(data) {
-				console.log(data);
-			});
-		}
-	}
-	function getOpenId(code, cb) {
-		// if (!weChat.getStorage('WXHNDTOPENID')) {
-		$.ajax({
-			type: 'GET',
-			url: 'https://a.weixin.hndt.com/boom/api/token/access/redirect2',
-			data: { code: code, cate: 'wx5f789dea59c6c2c5' },
-			dataType: 'json',
-			success: function(data) {
-				console.log(data);
-				if (data.status == 'ok') {
-					weChat.setStorage('WXHNDTOPENID', JSON.stringify(data.data));
-					cb && cb(data.data);
-				} else {
-					window.location = weChat.redirectUrl();
-				}
-			},
-			error: function(err) {
-				console.log(err);
-				window.location = weChat.redirectUrl();
-			}
-		});
-	}
+  //判断是否是微信客户端打开
+  if (weChat.isWeiXin()) {
+    var weChatCode = weChat.getQueryString('code')
+    if (weChatCode) {
+      getOpenId(weChatCode, function(data) {
+        console.log(data)
+      })
+    }
+  }
+  function getOpenId(code, cb) {
+    // if (!weChat.getStorage('WXHNDTOPENID')) {
+    $.ajax({
+      type: 'GET',
+      url: 'https://a.weixin.hndt.com/boom/api/token/access/redirect2',
+      data: { code: code, cate: 'wx5f789dea59c6c2c5' },
+      dataType: 'json',
+      success: function(data) {
+        console.log(data)
+        if (data.status == 'ok') {
+          weChat.setStorage('WXHNDTOPENID', JSON.stringify(data.data))
+          cb && cb(data.data)
+        } else {
+          window.location = weChat.redirectUrl()
+        }
+      },
+      error: function(err) {
+        console.log(err)
+        window.location = weChat.redirectUrl()
+      }
+    })
+  }
 
-	// 填充列表
-	$.ajax({
-		type: 'GET',
-		url: 'https://a.weixin.hndt.com/h5/2018dianshang/data/index.json',
-		dataType: 'json',
-		success: function(data) {
-			$.ajax({
-				type: 'GET',
-				url: 'https://a.weixin.hndt.com/boom/api/battle/entrevoteshowlist',
-				dataType: 'json',
-				timeout: 5000,
-				success: function(voteList) {
-					for (var i = 0; i < data.length; i++) {
-						data[i].vote = 0;
-					}
-					var newList = resetList(data, voteList);
-					listToHtml(newList);
-					loading.hide();
-				},
-				error: function(err) {
-					weui.alert('网络错误！');
-					loading.hide();
-				}
-			});
-		},
-		error: function(err) {
-			console.log(err);
-			loading.hide();
-			weui.alert('网络错误！');
-		}
-	});
-	function resetList(list, voteList) {
-		var len = list.length;
+  // 填充列表
+  $.ajax({
+    type: 'GET',
+    url: 'https://a.weixin.hndt.com/h5/2018dianshang/data/index.json',
+    dataType: 'json',
+    success: function(data) {
+      $.ajax({
+        type: 'GET',
+        url: 'https://a.weixin.hndt.com/boom/api/battle/entrevoteshowlist',
+        dataType: 'json',
+        timeout: 5000,
+        success: function(voteList) {
+          for (var i = 0; i < data.length; i++) {
+            data[i].vote = 0
+          }
+          var newList = resetList(data, voteList)
+          listToHtml(newList)
+          loading.hide()
+        },
+        error: function(err) {
+          weui.alert('网络错误！')
+          loading.hide()
+        }
+      })
+    },
+    error: function(err) {
+      console.log(err)
+      loading.hide()
+      weui.alert('网络错误！')
+    }
+  })
+  function resetList(list, voteList) {
+    var len = list.length
 
-		for (key in voteList) {
-			for (var i = 0; i < len; i++) {
-				var item = list[i];
-				// console.log(item.id, key);
-				// console.log(item.id == key);
-				if (item.id == key) {
-					item.vote = voteList[key];
-					break;
-				}
-			}
-		}
-		// console.log(list);
-		return list;
-	}
-	function listToHtml(list) {
-		var listWrap = $('.g-bd .list-wrap');
-		var len = list.length;
-		var html = '';
-		for (var i = 0; i < len; i++) {
-			var item = list[i];
-			// html +=
-			// 	'<li class="list">' +
-			// 	'                <div class="avatar-wrap">' +
-			// 	'                    <img src="' +
-			// 	item.icon +
-			// 	'" alt="" class="avatar">' +
-			// 	'                </div>' +
-			// 	'                <div class="text-wrap">' +
-			// 	'                    <h3 class="name">' +
-			// 	item.title +
-			// 	'</h3>' +
-			// 	'                    <a href="https://a.weixin.hndt.com/h5/2018dianshang/vote/index.html?id=' +
-			// 	item.id +
-			// 	'" class="link">' +
-			// 	'                        <span class="icon"></span>' +
-			// 	'                        <span class="text">投票</span>' +
-			// 	'                    </a>' +
-			// 	'                </div>' +
-			// 	'            </li>';
+    for (key in voteList) {
+      for (var i = 0; i < len; i++) {
+        var item = list[i]
+        // console.log(item.id, key);
+        // console.log(item.id == key);
+        if (item.id == key) {
+          item.vote = voteList[key]
+          break
+        }
+      }
+    }
+    // console.log(list);
+    return list
+  }
+  function listToHtml(list) {
+    var listWrap = $('.g-bd .list-wrap')
+    var len = list.length
+    var html = ''
+    for (var i = 0; i < len; i++) {
+      var item = list[i]
+      // html +=
+      // 	'<li class="list">' +
+      // 	'                <div class="avatar-wrap">' +
+      // 	'                    <img src="' +
+      // 	item.icon +
+      // 	'" alt="" class="avatar">' +
+      // 	'                </div>' +
+      // 	'                <div class="text-wrap">' +
+      // 	'                    <h3 class="name">' +
+      // 	item.title +
+      // 	'</h3>' +
+      // 	'                    <a href="https://a.weixin.hndt.com/h5/2018dianshang/vote/index.html?id=' +
+      // 	item.id +
+      // 	'" class="link">' +
+      // 	'                        <span class="icon"></span>' +
+      // 	'                        <span class="text">投票</span>' +
+      // 	'                    </a>' +
+      // 	'                </div>' +
+      // 	'            </li>';
 
-			html +=
-				'<li class="list">' +
-				'                <div class="avatar-wrap">' +
-				'                    <a href="https://a.weixin.hndt.com/h5/2018dianshang/vote/index.html?id=' +
-				item.id +
-				'"><img src="' +
-				item.icon +
-				'" alt="" class="avatar"></a>' +
-				'                </div>' +
-				'                <div class="text-wrap">' +
-				'                    <h3 class="name">' +
-				item.title +
-				'</h3>' +
-				'                    <div class="ticket-wrap">' +
-				'                        <span class="ticket-num">票数:' +
-				item.vote +
-				'</span>' +
-				'                    </div>' +
-				'                </div>' +
-				'            </li>';
-		}
-		listWrap.html(html);
-	}
-})();
+      html +=
+        '<li class="list">' +
+        '                <div class="avatar-wrap">' +
+        '                    <a href="https://a.weixin.hndt.com/h5/2018dianshang/vote/index.html?id=' +
+        item.id +
+        '"><img src="' +
+        item.icon +
+        '" alt="" class="avatar"></a>' +
+        '                </div>' +
+        '                <div class="text-wrap">' +
+        '                    <h3 class="name">' +
+        item.title +
+        '</h3>' +
+        '                    <div class="ticket-wrap">' +
+        '                        <span class="ticket-num">票数:' +
+        item.vote +
+        '</span>' +
+        '                    </div>' +
+        '                </div>' +
+        '            </li>'
+    }
+    listWrap.html(html)
+  }
+
+  //tab 切换
+  $('.g-tab .item').click(function() {
+    var index = $(this).index()
+    var top = 0
+    var time = 0
+    $(this)
+      .addClass('z-curt')
+      .siblings()
+      .removeClass('z-curt')
+    if (index == 0) {
+      top = $('.g-m .title').offset().top
+      time = 500
+    } else {
+      top = $('.g-bd .title').offset().top
+      time = 800
+    }
+
+    $('html, body').animate(
+      {
+        scrollTop: top
+      },
+      time
+    )
+  })
+})()
