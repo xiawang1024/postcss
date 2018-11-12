230ef92928f62ecf13c1b8bcd5e66a463c3a3dd2
wangxia/work <xiawang1024@icloud.com>
2018-11-12 17:00:22 +0800

    feat:人物事迹

--- gulpfile.js
+++ gulpfile.js
@@ -1,192 +1,201 @@
-const gulp = require('gulp');
+const gulp = require('gulp')
 
-const del = require('del');
+const del = require('del')
 
-const postcss = require('gulp-postcss');
+const postcss = require('gulp-postcss')
 
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')
 
-const stylus = require('gulp-stylus');
+const stylus = require('gulp-stylus')
 
-const uglify = require('gulp-uglify');
+const uglify = require('gulp-uglify')
 
-const babel = require('gulp-babel');
+const babel = require('gulp-babel')
 
-const sourceMaps = require('gulp-sourcemaps');
+const sourceMaps = require('gulp-sourcemaps')
 
-const concat = require('gulp-concat');
+const concat = require('gulp-concat')
 
-const rev = require('gulp-rev');
+const rev = require('gulp-rev')
 
-const revRewrite = require('gulp-rev-rewrite');
+const revRewrite = require('gulp-rev-rewrite')
 
-const revDelete = require('gulp-rev-delete-original');
+const revDelete = require('gulp-rev-delete-original')
 
-const imagemin = require('gulp-imagemin');
+const imagemin = require('gulp-imagemin')
 
-const tinyPng = require('gulp-tinypng');
+const tinyPng = require('gulp-tinypng')
 
-const autoprefixer = require('autoprefixer'); //自动加上浏览器前缀
+const autoprefixer = require('autoprefixer') //自动加上浏览器前缀
 
-const cssnano = require('cssnano');
+const cssnano = require('cssnano')
 
-const cssNext = require('postcss-cssnext');
+const cssNext = require('postcss-cssnext')
 
-const aspectRatio = require('postcss-aspect-ratio-mini');
+const aspectRatio = require('postcss-aspect-ratio-mini')
 
-const viewportUnits = require('postcss-viewport-units');
+const viewportUnits = require('postcss-viewport-units')
 
-const postcssWriteSvg = require('postcss-write-svg');
+const postcssWriteSvg = require('postcss-write-svg')
 
 // UI设计稿750px宽，那么100vw = 750px，即1vw = 7.5px
-const pxToViewport = require('postcss-px-to-viewport'); // 代码中写px编译后转化成vm
+const pxToViewport = require('postcss-px-to-viewport') // 代码中写px编译后转化成vm
 
-const browserSync = require('browser-sync');
-const opn = require('opn');
+const browserSync = require('browser-sync')
+const opn = require('opn')
 
 // 定义path
-let baseDir = './src';
-let cssDir = `${baseDir}/css/*.scss`;
-let jsDir = `${baseDir}/js/*.js`;
-let imgDir = `${baseDir}/img/*.{png,jpg,gif,ico}`;
-let htmlDir = `${baseDir}/*.html`;
-let outDir = `${baseDir}/dist`;
+let baseDir = './src'
+let cssDir = `${baseDir}/css/*.scss`
+let jsDir = `${baseDir}/js/*.js`
+let imgDir = `${baseDir}/img/*.{png,jpg,gif,ico}`
+let htmlDir = `${baseDir}/*.html`
+let outDir = `${baseDir}/dist`
 let path = {
-	css: `${baseDir}/css/*.scss`,
-	img: `${baseDir}/img/*.{png,jpg,gif,ico}`,
-	js: `${baseDir}/js/*.js`,
-	html: `${baseDir}/*.html`,
-	dist: `${baseDir}/dist`
-};
+  css: `${baseDir}/css/*.scss`,
+  img: `${baseDir}/img/*.{png,jpg,gif,ico}`,
+  js: `${baseDir}/js/*.js`,
+  html: `${baseDir}/*.html`,
+  dist: `${baseDir}/dist`
+}
 
 gulp.task('wx-css', () => {
-	// console.log(aspectRatio)
-	const plugins = [
-		aspectRatio({}),
-		postcssWriteSvg({
-			utf8: false
-		}),
-		pxToViewport({
-			viewportWidth: 750, // 视窗的宽度，对应的是我们设计稿的宽度，一般是750
-			viewportHeight: 1334, // 视窗的高度，根据750设备的宽度来指定，一般指定1334，也可以不配置
-			unitPrecision: 3, // 指定`px`转换为视窗单位值的小数位数（很多时候无法整除）
-			viewportUnit: 'vw', // 指定需要转换成的视窗单位，建议使用vw
-			selectorBlackList: [ '.ignore', '.hairlines' ], // 指定不转换为视窗单位的类，可以自定义，可以无限添加,建议定义一至两个通用的类名
-			minPixelValue: 1, // 小于或等于`1px`不转换为视窗单位，你也可以设置为你想要的值
-			mediaQuery: false // 允许在媒体查询中转换`px`著作权归作者所有。
-		}),
-		viewportUnits({}),
-		autoprefixer({ browsers: [ 'last 2 version' ] }),
-		cssnano({
-			preset: 'advanced',
-			autoprefixer: false,
-			'postcss-zindex': false
-		})
-	];
-	return gulp
-		.src(cssDir)
-		.pipe(sourceMaps.init())
-		.pipe(sass())
-		.pipe(postcss(plugins))
-		.pipe(concat('app.css'))
-		.pipe(gulp.dest(`${outDir}/css/`));
-});
+  // console.log(aspectRatio)
+  const plugins = [
+    aspectRatio({}),
+    postcssWriteSvg({
+      utf8: false
+    }),
+    pxToViewport({
+      viewportWidth: 750, // 视窗的宽度，对应的是我们设计稿的宽度，一般是750
+      viewportHeight: 1334, // 视窗的高度，根据750设备的宽度来指定，一般指定1334，也可以不配置
+      unitPrecision: 3, // 指定`px`转换为视窗单位值的小数位数（很多时候无法整除）
+      viewportUnit: 'vw', // 指定需要转换成的视窗单位，建议使用vw
+      selectorBlackList: ['.ignore', '.hairlines'], // 指定不转换为视窗单位的类，可以自定义，可以无限添加,建议定义一至两个通用的类名
+      minPixelValue: 1, // 小于或等于`1px`不转换为视窗单位，你也可以设置为你想要的值
+      mediaQuery: false // 允许在媒体查询中转换`px`著作权归作者所有。
+    }),
+    viewportUnits({}),
+    autoprefixer({ browsers: ['last 2 version'] }),
+    cssnano({
+      preset: 'advanced',
+      autoprefixer: false,
+      'postcss-zindex': false
+    })
+  ]
+  return gulp
+    .src(cssDir)
+    .pipe(sourceMaps.init())
+    .pipe(sass())
+    .pipe(postcss(plugins))
+    .pipe(concat('app.css'))
+    .pipe(gulp.dest(`${outDir}/css/`))
+})
 
 gulp.task('wx-js', () => {
-	return (
-		gulp
-			.src([
-				`${baseDir}/js/qrcode.min.js`,
-				`${baseDir}/js/zepto.js`,
-				`${baseDir}/js/fastclick.js`,
-				`${baseDir}/js/weui.js`,
-				`${baseDir}/js/jweixin.js`,
-				`${baseDir}/js/wxshare.js`,
-				`${baseDir}/js/index.js`
-			])
-			.pipe(sourceMaps.init())
-			// .pipe(
-			// 	babel({
-			// 		presets: [ 'env' ]
-			// 	})
-			// )
-			.pipe(uglify())
-			.pipe(concat('app.js'))
-			.pipe(gulp.dest(`${outDir}/js/`))
-	);
-});
+  return (
+    gulp
+      .src([
+        `${baseDir}/js/qrcode.min.js`,
+        `${baseDir}/js/zepto.js`,
+        `${baseDir}/js/fastclick.js`,
+        `${baseDir}/js/weui.js`,
+        `${baseDir}/js/jweixin.js`,
+        `${baseDir}/js/finger.js`,
+        `${baseDir}/js/wxshare.js`,
+        `${baseDir}/js/index.js`
+      ])
+      .pipe(sourceMaps.init())
+      // .pipe(
+      // 	babel({
+      // 		presets: [ 'env' ]
+      // 	})
+      // )
+      .pipe(uglify())
+      .pipe(concat('app.js'))
+      .pipe(gulp.dest(`${outDir}/js/`))
+  )
+})
 
 gulp.task('wx-png', function() {
-	return gulp.src(imgDir).pipe(tinyPng('S2DiR0odohioVwKoGNivVMOdzRUEPpwI')).pipe(gulp.dest(`${outDir}/img/`));
-});
+  return gulp
+    .src(imgDir)
+    .pipe(tinyPng('S2DiR0odohioVwKoGNivVMOdzRUEPpwI'))
+    .pipe(gulp.dest(`${outDir}/img/`))
+})
 
 gulp.task('wx-img', function() {
-	return gulp
-		.src(imgDir)
-		.pipe(
-			imagemin({
-				progressive: false, //Boolean类型 默认:false 无损压缩图片
-				optimizationLevel: 5, //number类型 默认:3 取值范围:0-7(优化等级)
-				interlced: true, //Boolean类型 默认false 隔行扫描gif进行渲染
-				multipass: true //Boolean类型 默认false 多次优化svg到完全优化
-			})
-		)
-		.pipe(gulp.dest(`${outDir}/img/`));
-});
+  return gulp
+    .src(imgDir)
+    .pipe(
+      imagemin({
+        progressive: false, //Boolean类型 默认:false 无损压缩图片
+        optimizationLevel: 5, //number类型 默认:3 取值范围:0-7(优化等级)
+        interlced: true, //Boolean类型 默认false 隔行扫描gif进行渲染
+        multipass: true //Boolean类型 默认false 多次优化svg到完全优化
+      })
+    )
+    .pipe(gulp.dest(`${outDir}/img/`))
+})
 
 gulp.task('wx-html', function() {
-	return gulp
-		.src(htmlDir) //指定当前文件夹下的所有html文件
-		.pipe(gulp.dest(`${outDir}`)) //将压缩后的文件输出到build文件夹下
-		.pipe(browserSync.stream()); //自动打开浏览器
-});
+  return gulp
+    .src(htmlDir) //指定当前文件夹下的所有html文件
+    .pipe(gulp.dest(`${outDir}`)) //将压缩后的文件输出到build文件夹下
+    .pipe(browserSync.stream()) //自动打开浏览器
+})
 
 gulp.task('clean', () => {
-	return del([ `${outDir}/css/*`, `${outDir}/js/*` ]);
-});
+  return del([`${outDir}/css/*`, `${outDir}/js/*`])
+})
 
-gulp.task('revision', [ 'wx-css', 'wx-js', 'wx-html', 'clean' ], () => {
-	return gulp
-		.src(`${outDir}/**/*.{css,js}`)
-		.pipe(rev())
-		.pipe(revDelete()) // Remove the unrevved files
-		.pipe(gulp.dest(`${outDir}`))
-		.pipe(rev.manifest())
-		.pipe(gulp.dest(`${outDir}`));
-});
+gulp.task('revision', ['wx-css', 'wx-js', 'wx-html', 'clean'], () => {
+  return gulp
+    .src(`${outDir}/**/*.{css,js}`)
+    .pipe(rev())
+    .pipe(revDelete()) // Remove the unrevved files
+    .pipe(gulp.dest(`${outDir}`))
+    .pipe(rev.manifest())
+    .pipe(gulp.dest(`${outDir}`))
+})
 
-gulp.task('revRewrite', [ 'revision' ], function() {
-	const manifest = gulp.src(`${outDir}/rev-manifest.json`);
+gulp.task('revRewrite', ['revision'], function() {
+  const manifest = gulp.src(`${outDir}/rev-manifest.json`)
 
-	return gulp.src(`${outDir}/index.html`).pipe(revRewrite({ manifest })).pipe(gulp.dest(`${outDir}`));
-});
+  return gulp
+    .src(`${outDir}/index.html`)
+    .pipe(revRewrite({ manifest }))
+    .pipe(gulp.dest(`${outDir}`))
+})
 
 // 任务列表
 
-const TASK = [ 'wx-img', 'revRewrite' ];
+const TASK = ['wx-img', 'revRewrite']
 
 gulp.task('default', TASK, function() {
-	//打开静态服务器
-	browserSync.init(
-		{
-			server: {
-				baseDir: path.dist
-			},
-			port: 3000,
-			open: false
-		},
-		function() {
-			var homepage = 'http://localhost:3000/';
-			opn(homepage);
-		}
-	);
+  //打开静态服务器
+  browserSync.init(
+    {
+      server: {
+        baseDir: path.dist
+      },
+      port: 3000,
+      open: false
+    },
+    function() {
+      var homepage = 'http://localhost:3000/'
+      opn(homepage)
+    }
+  )
 
-	//监听文件的变化实时编译 然后刷新
-	const watcher = gulp.watch([ path.html, path.js, path.css, path.img ], TASK);
-	watcher.on('change', function(event) {
-		console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-		setTimeout(() => {
-			browserSync.reload();
-		}, 2000);
-	});
-});
+  //监听文件的变化实时编译 然后刷新
+  const watcher = gulp.watch([path.html, path.js, path.css, path.img], TASK)
+  watcher.on('change', function(event) {
+    console.log(
+      'File ' + event.path + ' was ' + event.type + ', running tasks...'
+    )
+    setTimeout(() => {
+      browserSync.reload()
+    }, 2000)
+  })
+})
--- src/js/index.js
+++ src/js/index.js
@@ -1,168 +1,195 @@
 !(function() {
-	var loading = weui.loading('加载中...');
-	if (weChat.isPhone()) {
-		// fastClick 消除click 300ms延迟
-		if ('addEventListener' in document) {
-			document.addEventListener(
-				'DOMContentLoaded',
-				function() {
-					FastClick.attach(document.body);
-				},
-				false
-			);
-		}
-	}
+  var loading = weui.loading('加载中...')
+  if (weChat.isPhone()) {
+    // fastClick 消除click 300ms延迟
+    if ('addEventListener' in document) {
+      document.addEventListener(
+        'DOMContentLoaded',
+        function() {
+          FastClick.attach(document.body)
+        },
+        false
+      )
+    }
+  }
 
-	if (!weChat.isWeiXin()) {
-		new QRCode(document.getElementById('qrcode'), {
-			text: window.location.href,
-			width: 160,
-			height: 160
-		});
-	}
+  if (!weChat.isWeiXin()) {
+    new QRCode(document.getElementById('qrcode'), {
+      text: window.location.href,
+      width: 160,
+      height: 160
+    })
+  }
 
-	if (weChat.isWeiXin()) {
-		var weChatCode = weChat.getQueryString('code');
-		if (weChatCode) {
-			getOpenId(weChatCode, function(data) {
-				console.log(data);
-			});
-		}
-	}
-	function getOpenId(code, cb) {
-		// if (!weChat.getStorage('WXHNDTOPENID')) {
-		$.ajax({
-			type: 'GET',
-			url: 'https://a.weixin.hndt.com/boom/api/token/access/redirect2',
-			data: { code: code, cate: 'wx5f789dea59c6c2c5' },
-			dataType: 'json',
-			timeout: 5000,
-			success: function(data) {
-				console.log(data);
-				if (data.status == 'ok') {
-					weChat.setStorage('WXHNDTOPENID', JSON.stringify(data.data));
-					cb && cb(data.data);
-				} else {
-					window.location = weChat.redirectUrl();
-				}
-			},
-			error: function(err) {
-				console.log(err);
-				window.location = weChat.redirectUrl();
-			}
-		});
-		// }
-	}
+  if (weChat.isWeiXin()) {
+    var weChatCode = weChat.getQueryString('code')
+    if (weChatCode) {
+      getOpenId(weChatCode, function(data) {
+        console.log(data)
+      })
+    }
+  }
+  function getOpenId(code, cb) {
+    // if (!weChat.getStorage('WXHNDTOPENID')) {
+    $.ajax({
+      type: 'GET',
+      url: 'https://a.weixin.hndt.com/boom/wx/access/subscribe',
+      data: { code: code, state: 'wx5f789dea59c6c2c5', subscribe: false },
+      dataType: 'json',
+      timeout: 5000,
+      success: function(data) {
+        console.log(data)
+        if (data.status == 'ok') {
+          weChat.setStorage('WXHNDTOPENID', JSON.stringify(data.data))
+          cb && cb(data.data)
+        } else {
+          window.location = weChat.redirectUrl()
+        }
+      },
+      error: function(err) {
+        console.log(err)
+        window.location = weChat.redirectUrl()
+      }
+    })
+    // }
+  }
 
-	//个人信息
-	// var url = 'https://api.hndt.com/api/page?template_id=357&article_id=' + weChat.getQueryString('id');
-	var url = 'https://a.weixin.hndt.com/h5/2018dianshang/data/' + weChat.getQueryString('id') + '.json';
-	$.ajax({
-		type: 'GET',
-		url: url,
-		dataType: 'json',
-		timeout: 5000,
-		success: function(data) {
-			toHtml(data);
-			//视频播放
+  //uuid生成
+  if (window.requestIdleCallback) {
+    requestIdleCallback(function() {
+      fingerHash()
+    })
+  } else {
+    setTimeout(function() {
+      fingerHash()
+    }, 500)
+  }
+  var uuid = null
+  function fingerHash() {
+    Fingerprint2.get(function(components) {
+      var murmur = Fingerprint2.x64hash128(
+        components
+          .map(function(pair) {
+            return pair.value
+          })
+          .join(),
+        31
+      )
+      uuid = murmur
+      console.log(murmur)
+    })
+  }
 
-			//投票
-			$('#vote-btn').click(function() {
-				var userInfo = JSON.parse(weChat.getStorage('WXHNDTOPENID'));
-				if (!userInfo) {
-					weui.alert('请打开微信投票！');
-					return;
-				}
-				var voteLoading = weui.loading('努力提交中...');
-				$.ajax({
-					type: 'POST',
-					url: 'https://a.weixin.hndt.com/boom/api/battle/entrevoteadd',
-					dataType: 'json',
-					timeout: 5000,
-					data: {
-						title: data.title,
-						ref: 'https://a.weixin.hndt.com/h5/2018dianshang/vote/index.html?id=' + data.id,
-						openId: userInfo.openid,
-						mobile: '',
-						code: weChat.getQueryString('code'),
-						userId: '',
-						id: data.id
-					},
-					success: function(data) {
-						if (data.status == 'ok') {
-							//投票信息
-							$.ajax({
-								type: 'post',
-								url: 'https://a.weixin.hndt.com/boom/api/battle/entrevoteshow',
-								data: {
-									id: weChat.getQueryString('id')
-								},
-								dataType: 'json',
-								success: function(data) {
-									$('.g-bd .ticket-num').html('票数：' + data.data + '');
-								},
-								error: function(err) {
-									console.log(err);
-								}
-							});
-							voteLoading.hide();
-							weui.toast('投票成功！');
-						} else {
-							voteLoading.hide();
-							// weui.alert('投票于7月19日09时30分正式开启！');
-							weui.alert('今日投票次数用完，明日再投！');
-						}
-					},
-					error: function(err) {
-						console.log(err);
-						voteLoading.hide();
-						weui.alert('网络错误！');
-					}
-				});
-			});
+  //个人信息
+  // var url = 'https://api.hndt.com/api/page?template_id=357&article_id=' + weChat.getQueryString('id');
+  // var url = 'https://a.weixin.hndt.com/h5/2018dianshang/data/' + weChat.getQueryString('id') + '.json';
+  var url = 'http://192.168.9.41:3000/index.json'
+  $.ajax({
+    type: 'GET',
+    url: url,
+    dataType: 'json',
+    timeout: 5000,
+    success: function(data) {
+      toHtml(data)
+      voteHandler(data)
+    }
+  })
+  //投票
+  function voteHandler(id) {
+    $('#vote-btn').click(function() {
+      var userInfo = JSON.parse(weChat.getStorage('WXHNDTOPENID'))
+      if (!userInfo) {
+        weui.alert('请打开微信投票！')
+        return
+      }
+      var voteLoading = weui.loading('努力提交中...')
 
-			$('#video-play').click(function() {
-				document.getElementById('video').play();
-				$(this).hide();
-			});
-		},
-		error: function(err) {
-			console.log(err);
-			loading.hide();
-			weui.alert('网络错误！');
-		}
-	});
+      var appId = 'wx5f789dea59c6c2c5',
+        voteId = 3
+      var openId = userInfo.openid
 
-	function toHtml(data) {
-		$('.g-bd .avatar').attr('src', data.icon);
-		$('.g-bd .name').html(data.title);
-		$('.video').attr('src', data.video);
-		var body = data.body.replace(/src/g, 'data');
-		$('.g-bdc .content').html(body);
-		// $('.g-bdc .content').html(data.body);
-		setTimeout(function() {
-			$('.content strong').css('color', '#2481c5');
-			$('.content strong').eq(0).css('color', '#000');
-		}, 20);
-	}
+      $.ajax({
+        type: 'POST',
+        url: 'https://a.weixin.hndt.com/boom/openapi/vote/log/add',
+        dataType: 'json',
+        timeout: 5000,
+        data: {
+          appId: appId,
+          openId: openId,
+          voteId: voteId,
+          id: id,
+          uuid: uuid
+        },
+        success: function(data) {
+          voteLoading.hide()
+          if (data.status == 'ok') {
+            weui.alert('投票成功！')
+            refreshVote()
+          } else if (data.status == 'warn') {
+            weui.alert('投票未开始！')
+          } else {
+            weui.alert('投票失败！')
+          }
+        },
+        error: function(err) {
+          console.log(err)
+          voteLoading.hide()
+          weui.alert('网络错误！')
+        }
+      })
+    })
+  }
+  //更新投票数
+  function refreshVote() {
+    //投票信息
+    $.ajax({
+      type: 'post',
+      url: 'https://a.weixin.hndt.com/boom/api/battle/entrevoteshow',
+      data: {
+        id: weChat.getQueryString('id')
+      },
+      dataType: 'json',
+      success: function(data) {
+        $('.g-bd .ticket-num').html('票数：' + data.data + '')
+      },
+      error: function(err) {
+        console.log(err)
+      }
+    })
+  }
+  //toHtml
+  function toHtml(data) {
+    $('.g-bd .avatar').attr('src', data.icon)
+    $('.g-bd .name').html(data.title)
+    $('.video').attr('src', data.video)
+    var body = data.body.replace(/src/g, 'data')
+    $('.g-bdc .content').html(body)
+    // $('.g-bdc .content').html(data.body);
+    setTimeout(function() {
+      $('.content strong').css('color', '#2481c5')
+      $('.content strong')
+        .eq(0)
+        .css('color', '#000')
+    }, 20)
+  }
 
-	//投票信息
-	$.ajax({
-		type: 'post',
-		url: 'https://a.weixin.hndt.com/boom/api/battle/entrevoteshow',
-		data: {
-			id: weChat.getQueryString('id')
-		},
-		dataType: 'json',
-		timeout: 10000,
-		success: function(data) {
-			loading.hide();
-			$('.g-bd .ticket-num').html('票数：' + data.data + '');
-		},
-		error: function(err) {
-			console.log(err);
-			loading.hide();
-			weui.alert('网络错误！');
-		}
-	});
-})();
+  //更新投票数
+  $.ajax({
+    type: 'post',
+    url: 'https://a.weixin.hndt.com/boom/api/battle/entrevoteshow',
+    data: {
+      id: weChat.getQueryString('id')
+    },
+    dataType: 'json',
+    timeout: 10000,
+    success: function(data) {
+      loading.hide()
+      $('.g-bd .ticket-num').html('票数：' + data.data + '')
+    },
+    error: function(err) {
+      console.log(err)
+      loading.hide()
+      weui.alert('网络错误！')
+    }
+  })
+})()
