ab0b12a5b15c42438f35f9e323f67c8bd706ff73
wangxia/work <xiawang1024@icloud.com>
2019-01-10 15:20:22 +0800

    河南公安十大出彩人物

--- src/js/wxshare.js
+++ src/js/wxshare.js
@@ -1,53 +1,51 @@
 window.onload = function() {
-  var TITLE = '2018“感动中原”十大年度人物网络投票活动'
-  var LINK = 'https://a.weixin.hndt.com/h5/gdzy/index.html' //分享链接
-  var IMG_URL = 'https://a.weixin.hndt.com/h5/gdzy/icon-share.png'
-  var DESC = '2018“感动中原”十大年度人物网络投票活动'
+	var TITLE = '十大出彩河南公安人物网络投票'
+	var LINK = 'https://a.weixin.hndt.com/h5/hnga/index.html' //分享链接
+	var IMG_URL = 'https://a.weixin.hndt.com/h5/hnga/icon-share.png'
+	var DESC = '十大出彩河南公安人物网络投票'
 
-  //微信配置
-  var href = window.location.href
-  $.post('https://a.weixin.hndt.com/boom/at/sign', { url: href }, function(
-    data
-  ) {
-    wx.config({
-      debug: false,
-      appId: data.appId,
-      timestamp: data.timestamp,
-      nonceStr: data.nonceStr,
-      signature: data.signature,
-      jsApiList: [
-        'onMenuShareTimeline',
-        'onMenuShareAppMessage',
-        'chooseImage',
-        'previewImage',
-        'startRecord',
-        'playVoice',
-        'stopRecord',
-        'downloadVoice',
-        'uploadVoice',
-        'stopVoice',
-        'openLocation'
-      ]
-    })
-  })
+	//微信配置
+	var href = window.location.href
+	$.post('https://a.weixin.hndt.com/boom/at/sign', { url: href }, function(data) {
+		wx.config({
+			debug: false,
+			appId: data.appId,
+			timestamp: data.timestamp,
+			nonceStr: data.nonceStr,
+			signature: data.signature,
+			jsApiList: [
+				'onMenuShareTimeline',
+				'onMenuShareAppMessage',
+				'chooseImage',
+				'previewImage',
+				'startRecord',
+				'playVoice',
+				'stopRecord',
+				'downloadVoice',
+				'uploadVoice',
+				'stopVoice',
+				'openLocation'
+			]
+		})
+	})
 
-  wx.ready(function() {
-    wx.onMenuShareTimeline({
-      title: TITLE,
-      link: LINK,
-      imgUrl: IMG_URL,
-      success: function() {},
-      cancel: function() {}
-    })
-    wx.onMenuShareAppMessage({
-      title: TITLE,
-      desc: DESC,
-      link: LINK,
-      imgUrl: IMG_URL,
-      type: '',
-      dataUrl: '',
-      success: function() {},
-      cancel: function() {}
-    })
-  })
+	wx.ready(function() {
+		wx.onMenuShareTimeline({
+			title: TITLE,
+			link: LINK,
+			imgUrl: IMG_URL,
+			success: function() {},
+			cancel: function() {}
+		})
+		wx.onMenuShareAppMessage({
+			title: TITLE,
+			desc: DESC,
+			link: LINK,
+			imgUrl: IMG_URL,
+			type: '',
+			dataUrl: '',
+			success: function() {},
+			cancel: function() {}
+		})
+	})
 }
